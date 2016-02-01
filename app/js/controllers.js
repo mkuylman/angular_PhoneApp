4f97e342daa99a7fab4407a2ab4ba8d3ade16b7a
mkuylman <mkuylman@gmail.com>
2016-02-01 12:56:50 -0800

    Pushing app to Heroku

---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+var phoneApp = angular.module('phoneApp', []);
+
+phoneApp.controller('PhoneListCtrl', ['$scope', '$http',
+function($scope, $http){
+	$http.get('phones/phones.json').success(function(data) {
+		$scope.phones = data;
+	});
+
+
+  $scope.orderProp = '-age';
+}]);
